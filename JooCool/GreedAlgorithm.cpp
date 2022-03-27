f3143a5483ab32e6997aa31a41559957af67d93e
joosiwon <90442015+joosiwon@users.noreply.github.com>
2022-03-27 19:03:07 +0900

    그리드 알고리즘 회의실 배정

--- JooCool/GreedAlgorithm.cpp
+++ JooCool/GreedAlgorithm.cpp
@@ -3,8 +3,7 @@
 // sort(시작시간 오름차순) 으로 정렬  
 // 같은 시간일떄 끝나는 시간 오름차순 정렬
 
-
-#include<cstdio>
+#include <iostream>
 #include<algorithm>
 #include<queue>
 #include<functional>
@@ -17,16 +16,16 @@ int main()
 {
 	int cnt = 0;
 	int n;
-	scanf_s("%d", &n);
+	cin >> n;
 	intpair* v = new intpair[n + 1];
 	for (int i = 1; i <= n; i++)
 	{
-		scanf_s("%d %d", &v[i].first, &v[i].second); // 회의 시작시간 끝시간들을 n번 입력받음
+		cin >> v[i].first, v[i].second; // 회의 시작시간 끝시간들을 n번 입력받음
 	}
 	sort(v, v + n + 1);
 	priority_queue<int, vector<int>, greater<int>>pq; // 우선순위 큐를 사용해서 정렬
 	pq.push(v[0].second); //정렬을 했으니까, 맨 처음끝나는 시간을 넣는다.
-	
+
 	int start, end;
 	for (int i = 1; i <= n; i++)
 	{
@@ -35,13 +34,13 @@ int main()
 		if (pq.top() > start)
 		{
 			pq.push(end);
-	}	//수업중인 강의실의 끝나는 시간 > 다음 수업 시작시간 => 강의실 추가
+		}	//수업중인 강의실의 끝나는 시간 > 다음 수업 시작시간 => 강의실 추가
 		else
 		{
 			pq.pop();	// 아니면 팝
 			pq.push(end);
 		}
 	}
-	printf("%d\n", pq.size());
+	cout << pq.size() << endl;
 	return 0;
 }
